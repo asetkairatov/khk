cd623884dfb880edfafaf262f88daf79226bea17
asetkairatov <kairatovaset2706@gmail.com>
2024-10-09 14:56:08 +0500

    blablabla

---
+++ MainApplication.java
@@ -0,0 +1,26 @@
+package kz.aset.first.project;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.sql.SQLOutput;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("salam");
+//        System.out.println("комменатрии делают через //");
+//        System.out.println("Ctrl + /");
+        int a = 10;
+        Float d = 15.5f;
+        boolean h = false;
+        System.out.println(a *=3);
+//        System.out.println(a + 2);
+//        String c = "BOMB";
+//        System.out.println(c);
+//        System.out.println(d);
+        System.out.println(h);
+    }
+
+    ;
+}
+
---
+++ popytkaDva.java
@@ -0,0 +1,67 @@
+package kz.aset.first.project;
+
+public class popytkaDva {
+    public static void main(String[] args) {
+        drawLol();
+        love();
+        cat();
+        cat();
+sumAndPrint();
+temperature();
+    }
+
+    public static void drawLol() {
+        System.out.println("╱╱▏┈┈╱╱╱╱▏╱╱");
+        System.out.println("▇╱▏┈┈▇▇▇╱▏▇╱▏");
+        System.out.println("▇╱▏▁┈▇╱▇╱▏▇╱");
+        System.out.println("▇╱╱╱▏▇╱▇╱▏▇╱╱╱");
+        System.out.println("▇▇▇╱┈▇▇▇╱┈▇▇▇╱");
+
+    }
+
+    public static void love() {
+        System.out.println("|||||||||||||||||||||||||||||||||||");
+        System.out.println(" ▓▓▓█──.  ▓▓▓▓    ▓█────▓▓   █▓▓▓▓█");
+        System.out.println("─▓▓██── ▓▓█──▓▓█  ▓▓█──▓▓█  ─▓▓█");
+        System.out.println("─▓▓█─── ▓▓█──▓▓█─ ▓▓█─▓▓█─   ▓▓▓▓▓█");
+        System.out.println("─▓▓█── ─▓▓█──▓▓█─ ─▓▓.▓▓█─- ─▓▓█");
+        System.out.println("─▓▓▓▓▓█─  ▓▓▓▓──    ▓▓▓─    ─▓▓▓▓▓█");
+    }
+
+    public static void cat() {
+        System.out.println("　　　　 .,,..;~`””　　　　`””＜“彡　}");
+        System.out.println("　    _…:=,`’　　 　︵　 т　︵　　X彡-J");
+        System.out.println("     ＜`　彡 /　　ミ　　,_人_.　＊彡　`~");
+        System.out.println("　      `~=::　　   　　　　　　 　　　Y");
+        System.out.println("　　       　i.　　　　　　　　　　　　 .:");
+        System.out.println("　      　　.\\　　　　　　　,｡—.,,　　./");
+        System.out.println("　　　        　ヽ　／ﾞ”“`\\;.{　　　 ＼／");
+        System.out.println(" 　        　　　　Y　　　`J..r_.彳　 　|");
+        System.out.println("　　         　　　{　　　“　　`　　　i");
+        System.out.println("　　　         　　\\　　　　　　　　　＼　　 ..︵︵.");
+        System.out.println("　　　         　　`＼　　　　　　　　　“ゞ.,/` oQ o`)");
+        System.out.println("　　           　　　　`i,　　　　　　　　　　Y　 ω　/");
+        System.out.println("　　　         　 　　`i,　　　 　　.　　　　»　　　/");
+        System.out.println("　　　            　　`iミ　　　　　　　　　　　,,ノ");
+        System.out.println("　　　　        　 ︵Y..︵.,,　　　　　,,+..__ノ“");
+        System.out.println("　　　          　　(,`, З о　　　　,.ノ川彡ゞ彡…");
+        System.out.println('-');
+    }
+
+    public static void sumAndPrint() {
+        int a = 10;
+        int b = 17;
+        int sum = a + b;
+        System.out.println("сумма:" + sum);
+    }
+
+    public static void temperature() {
+        int t = 30;
+        if (t < -10) {
+            System.out.println("следует одеть куртку");
+        }
+        if (t > 25) {
+            System.out.println("следует одеть майку");
+        }
+    }
+}
